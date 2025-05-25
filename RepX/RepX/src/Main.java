90502ff0cc012b2df8b436f8b1d996b2f00407b2
NamanDev01 <naman.24scse1420146@galgotiasuniversity.ac.in>
2025-05-26 01:28:40 +0530

    Add files via upload

---
+++ RepX/RepX/src/Main.java
@@ -0,0 +1,7 @@
+import repx.view.UserTypeSelectionFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(UserTypeSelectionFrame::new);
+    }
+}
