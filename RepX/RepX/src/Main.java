e7e2efd4e63919ec80580c5ad97aa08de7d6a184
NAMAN OJHA <naman.24scse1420146@galgotiasuniversity.ac.in>
2025-06-10 23:44:26 +0530

    Delete RepX/RepX directory

--- RepX/RepX/src/Main.java
+++
@@ -1,7 +0,0 @@
-import repx.view.UserTypeSelectionFrame;
-
-public class Main {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(UserTypeSelectionFrame::new);
-    }
-}
